ed460b4d870d55bbb7192cf82227569281e852c4
paullecomte07 <paullecomte07@gmail.com>
2020-04-10 17:12:25 +0100

    first running version of the app

--- rpiserver/rpiserver/server.py
+++ rpiserver/rpiserver/server.py
@@ -9,37 +9,60 @@ GPIO.setwarnings(False)
 
 
 
+class Comment(BaseModel):
+    pseudo : str
+    comment : str
 
-class Valve(BaseModel):
+
+class Led(BaseModel):
+    id : int
     gpio : int
-    is_open : bool = False
+    is_on : bool = False
+    color : str
 
-def init(self):
+    def init(self):
         GPIO.setup(self.gpio, GPIO.OUT)
         return self
-def open(self):
+    def turnOn(self):
         GPIO.output(self.gpio, GPIO.HIGH)
-        self.is_open = True
-def close(self):
+        self.is_on = True
+    def turnOff(self):
         GPIO.output(self.gpio, GPIO.LOW)
-        self.is_open = False
-
-
-valve = Valve(gpio=19).init()
+        self.is_on = False
 
 
 
 
+db = []
 
-valve_app = FastAPI()
-@valve_app.on_event("shutdown")
-def close_valves():
-    valve.close()
-@valve_app.get("/valves/open", response_model=Valve)
-def open_valve(id:int):
-    valve.open()
-    return valve
-@valve_app.get("/valves/close", response_model=Valve)
-def close_valve():
-    valve.close()
-    return valve
+
+
+led_app = FastAPI()
+
+
+@led_app.post("/led/create", response_model=Led)
+def create_led(led: Led):
+    led.init()
+    db.append(led)
+    return led
+
+@led_app.get("/led/{led_id}", response_model=Led)
+def get_led(led_id: int):
+    for i in db:
+        if i.id ==led_id:
+            return i
+
+@led_app.post("/led/{led_id}/on")
+def turnOn(led_id:int):
+    for i in db:
+        if i.id == led_id:
+            i.turnOn()
+            return i
+
+@led_app.post("/led/{led_id}/off")
+def turnOff(led_id:int):
+    for i in db:
+        if i.id == led_id:
+            i.turnOff()
+            return i
+
